430b518d382d03655aaf561163217ff4f79a03f6
andres-cruz10223 <andrescruz221996@gmail.com>
2024-08-06 15:04:42 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion=$_POST['Identificacion']:
+  $NomApellidos =$_POST['NomApellidos];
+  $Cargo=$_POST['Cargo'];
+  $Salario=_POST['Salario'];
+  echo "La identificacion del empleado es : ".Identificacion;
+  echo "Los Nombres y Apellidos del empleado son :".NomApellidos;
+  echo "El salario del empleado es: ".Salario;
+  echo "El cargo del empleado es :".Cargo;
+?>
